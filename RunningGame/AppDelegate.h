bf5704a14d310a0a56029a2d28b75e4204cc4f24
NSPostWhenIdle <maccdevteam@gmail.com>
2014-03-28 18:13:07 -0400

    Initial Commit

---
+++ RunningGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RunningGame
+//
+//  Created by Mick on 3/28/14.
+//  Copyright (c) 2014 MacCDevTeam LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RunningGameTests/RunningGameTests.m
@@ -0,0 +1,34 @@
+//
+//  RunningGameTests.m
+//  RunningGameTests
+//
+//  Created by Mick on 3/28/14.
+//  Copyright (c) 2014 MacCDevTeam LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RunningGameTests : XCTestCase
+
+@end
+
+@implementation RunningGameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
